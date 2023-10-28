ee6ef39b7a6dfd3c57099529a35efafe312c5e4a
Tomas E <tomas.echeverria17293@alumnos.info.unlp.edu.ar>
2023-10-28 17:11:27 -0300

    actualizacion gemspec

--- lib/ruby_2023.rb
+++ lib/ruby_2023.rb
@@ -4,5 +4,33 @@ require_relative "ruby_2023/version"
 
 module Ruby2023
   class Error < StandardError; end
-  # Your code goes here...
+  module Countable
+    module ClassMethods
+      def count_invocations_of(*method_names)
+        method_names.each do |method_name|
+          alias_method "orig_#{method_name}", method_name
+          define_method(method_name) do
+            invocation_count[method_name] += 1
+            send("orig_#{method_name}")
+          end
+        end
+      end
+    end
+  
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+  
+    def invocation_count
+      @invocation_count ||= Hash.new(0)
+    end
+  
+    def invoked?(method_name)
+      invoked(method_name) > 0
+    end
+  
+    def invoked(method_name)
+      invocation_count[method_name]
+    end
+  end
 end
